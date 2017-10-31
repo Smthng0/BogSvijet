91734a1b4c5554b505804b57c7cd4437bca18fe6
Smthng0 <Anonymus2304@gmail.com>
2017-10-31 11:28:08 +0100

    Marko was here...

--- src/main/java/dreamfactory/learning/bogsvijet/BogSvijet.java
+++ src/main/java/dreamfactory/learning/bogsvijet/BogSvijet.java
@@ -1,7 +1,20 @@
 package  dreamfactory.learning.bogsvijet;
 
+import dreamfactory.learning.lib.Zec;
+
 public class BogSvijet {
     public static void main(String[] args) {
         System.out.println("Bobo smrade");
+        Zec zec = new Zec();
+
+        for (String argument : args) {
+            System.out.print(argument);
+        }
+        System.out.println();
     }
+
+    public static int bla(){
+        return 2;
+    }
+
 }
---
+++ src/main/java/dreamfactory/learning/lib/Zec.java
@@ -0,0 +1,4 @@
+package dreamfactory.learning.lib;
+
+public class Zec {
+}
